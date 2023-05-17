66f8b5a6529ee0da19a5af5b1ccfaf677702aaab
DELL <rk.codge990@gmail.com>
2023-05-17 02:51:28 -0700

    update operator

---
+++ byte_range.java
@@ -0,0 +1,8 @@
+public class byte_range {
+    public static void main(string[]args){
+        byte b=12;
+        byte b1=120;
+        system.out.println(b);
+    }
+
+}
---
+++ src/evenodd.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.util.Scanner;
+public class evenodd {
+    public static void main(string[] arg)
+    {
+        int n;
+    Scanner S = new Scanner(System.in);
+    system.out.print("Enter the number even or odd:");
+    n=s.nextInt();
+    if(n%2 ==0)
+
+    {
+        sytem.out.println("the given number "+n+"is Even");
+
+    }
+    else
+
+    {
+        sytem.out.println("the given number " +n+"is odd");
+    }
+}
+}
